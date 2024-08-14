f9ef243b3eecb9e9da9a1d9f326b6056866b708b
Juna <46632782+JunaMeinhold@users.noreply.github.com>
2024-08-14 17:32:54 +0200

    Add project files.

---
+++ Generator/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HexaGen;
+
+CsCodeGeneratorSettings settings = CsCodeGeneratorSettings.Load("generator.json");
+CsCodeGenerator generator = new(settings);
+generator.Generate(["include/main.h"], "../../../../Hexa.NET.SDL2/Generated");
